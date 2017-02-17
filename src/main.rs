47e7244a50fda5e848cc9542801e77dc9f53d1a9
Jason Kleban <jason.kleban@outlook.com>
2017-02-16 21:02:51 -0500

    playing with Clap and Problem 1

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,21 @@
-fn main() {
-    println!("Hello again, everybody!");
+extern crate clap;
 
-    println!()
+use clap::{App, Arg};
+
+fn main() {
+    let matches = App::new("RustyEuler")
+        .version("0.0.1")
+        .author("Jason Kleban")
+        .about("Project Euler problems in Rust")
+        .arg(Arg::with_name("r")
+            .short("r")
+            .long("run")
+            .takes_value(true)
+            .multiple(true))
+        .get_matches();
+
+        match matches.occurrences_of("r")
+        {
+            1 | _ => println!("Problem 1: {0}", (1..1000).filter(|x| x % 3 == 0 || x % 5 == 0).fold(0, |a, i| a + i))
+        }
 }
